33f18a50220d7dcb8ad9004f6a6e336ee4d7b3d1
illancapan <igorllancapan@gmail.com>
2024-06-28 19:08:43 -0400

    first commit

---
+++ database/config.js
@@ -0,0 +1,18 @@
+import pg from "pg";
+
+const pool = new pg.Pool({
+    host: "localhost",
+    user: "postgres",
+    password: "b00tcamp",
+    database: "likeme",
+    allowExitOnIdle: true,
+});
+
+pool.query("SELECT NOW()", (err, res) => {
+if(err) {
+    console.log('Error en conectar a base de datos😒😒, err')
+} else {
+  console.log('Base de datos conectada😊')}
+})
+
+export default pool;
