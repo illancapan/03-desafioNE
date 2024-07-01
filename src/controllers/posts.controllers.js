23a09448b81042b8c725ef4a342d447afab1c22b
illancapan <igorllancapan@gmail.com>
2024-06-30 21:01:45 -0400

    funcionando

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -6,22 +6,72 @@ import {
 export const getAllPost = async (req, res) => {
     try {
         const posts = await getAllPostsModels();
-        res.status(200).json({ posts: result.rows });
-        res.json(posts);
+        res.status(200).json(posts);
     } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: error.message });
+        console.error(error);
+        res.status(500).json({ error: 'Error interno del servidor' });
     }
 };
 
 export const postAllPost = async (req, res) => {
     const { titulo, img, descripcion, likes } = req.body;
     try {
-        const posts = await postAllPostsModels(titulo, img, descripcion, likes);
-        res.status(200).json({ posts: result.rows });
-        res.json(posts);
+        const nuevoPost = await postAllPostsModels(titulo, img, descripcion, likes);
+        res.status(200).json({ post: nuevoPost });
     } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: error.message });
+        console.error('Error al crear el post:', error);
+        res.status(500).json({ error: 'Error interno del servidor' });
     }
 };
+
+
+
+// import {
+//     getAllPostsModels,
+//     postAllPostsModels,
+// } from "../models/posts.models.js";
+
+
+// // export const getAllPost = async (req, res) => {
+// //     try {
+// //         const posts = await getAllPostsModels();
+// //         res.status(200).json({ posts }); // Devuelve los posts obtenidos como JSON
+// //     } catch (error) {
+// //         console.log(error);
+// //         res.status(500).json({ error: error.message });
+// //     }
+// // };
+
+// export const getAllPost = async (req, res) => {
+//     try {
+//         const posts = await getAllPostsModels();
+//         res.status(200).json({ posts });
+//         // res.json(posts);
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({ error: error.message });
+//     }
+// };
+
+// export const postAllPost = async (req, res) => {
+//     const { titulo, img, descripcion, likes } = req.body;
+//     try {
+//         const nuevoPosts = await postAllPostsModels(titulo, img, descripcion, likes);
+//         res.status(200).json({post: nuevoPosts});
+//     } catch (error) {
+//         console.error('Error al crear el post:', error);
+//         res.status(500).json({ error: 'Error interno del servidor' });
+//     }
+// };
+
+// // export const postAllPost = async (req, res) => {
+// //     const { titulo, img, descripcion, likes } = req.body;
+// //     try {
+// //         const nuevoPosts = await postAllPostsModels(titulo, img, descripcion, likes);
+// //         res.status(200).json({ nuevoPosts: result.rows });
+// //         res.json(posts);
+// //     } catch (error) {
+// //         console.log(error);
+// //         res.status(500).json({ error: 'Error interno del servidor' error.message });
+// //     }
+// // };
--- src/models/posts.models.js
+++ src/models/posts.models.js
@@ -4,21 +4,22 @@ import pool from "../../database/config.js";
 export const getAllPostsModels = async () => {
     const SQLquery = { text: "SELECT * FROM posts" };
     const result = await pool.query(SQLquery);
-    return result.rows[0];
+    return result.rows;
 };
 
 // MODELO DE POSTS
 
-export const postAllPostsModels = async () => {
+export const postAllPostsModels = async (titulo, img, descripcion, likes) => {
     const sql =
         "INSERT INTO posts (titulo, img,descripcion,likes) VALUES ($1, $2, $3, $4) RETURNING *";
     const values = [titulo, img, descripcion, likes];
     try {
         const result = await pool.query(sql, values);
-        return result.rows[0];
+        // res.status(200).json(result.rows[0]);
+        return result.rows[0]; // retorna el nuevo post creado
     } catch (error) {
-        console.log(error.message)
-        trhow error;
-        
+        console.error("Error al crear el post:", error);
+        throw error;
+        // res.status(500).json({ error: error.message });
     }
 };
